ae028dd208b9ebaf59c2cd131229df5a45789f79
mayankkushal <mayankkushal26@gmail.com>
2017-03-29 00:43:27 +0530

    d

--- firepit_project/settings.py
+++ firepit_project/settings.py
@@ -108,13 +108,20 @@ TEMPLATES = [
     },
 ]
 
-
+WSGI_APPLICATION = 'firepit_project.wsgi.application'
 
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 #Haystack search
 HAYSTACK_CONNECTIONS = {
